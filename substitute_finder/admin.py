69e7d11bcdcf21cc2c71d4f55d1131d3dc6758d7
tom.gabriele <tom.gabriele@free.fr>
2018-07-31 22:23:12 +0200

    feat (get data): create models and admin
    - Product and Category models allow to store data from openfoodfacts
    - Favorites and linked categories are managed by many to many field
    
    Close https://trello.com/c/NSJXgRxX/13-create-models

--- substitute_finder/admin.py
+++ substitute_finder/admin.py
@@ -1,7 +1,39 @@
 from django.contrib import admin
-from .models import CustomUser
+from .models import CustomUser, Product, Category
 # Register your models here.
+
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
+    pass
+
+
+class CustomUserInline(admin.TabularInline):
+    model = Product.users.through
+    verbose_name = 'Utilistateur'
+    verbose_name_plural = 'Utilisateurs'
+
+
+class CategoryInline(admin.TabularInline):
+    model = Product.categories.through
+    verbose_name = 'Catégorie'
+    verbose_name_plural = 'Catégories'
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    search_fields = ['product_name','description']
+    list_filter = ['nutrition_grade','first_seller','categories']
+    inlines = [CustomUserInline, CategoryInline]
+    exclude = ('users', 'categories')
+
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['category_name',]
+
+
+@admin.register(Product.users.through)
+class FavoriteAdmin(admin.ModelAdmin):
     pass
--- substitute_finder/models.py
+++ substitute_finder/models.py
@@ -10,3 +10,37 @@ class CustomUser(AbstractUser):
     EMAIL_FIELD = 'email'
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
+
+
+class Product(models.Model):
+    product_name = models.CharField(
+        verbose_name='nom du produit', max_length=300)
+    description = models.CharField(verbose_name='description', max_length=1000)
+    open_food_facts_url = models.URLField(
+        verbose_name='url OpenFoodFacts', max_length=1000)
+    first_seller = models.CharField(verbose_name='vendeur', max_length=300)
+    nutrition_grade = models.CharField(
+        verbose_name='score nutritionnel', max_length=1)
+    last_updated = models.DateTimeField(
+        verbose_name='dernière mise à jour', auto_now=True)
+    categories = models.ManyToManyField(to='Category', verbose_name='categories')
+    users = models.ManyToManyField(to='CustomUser', related_name='favorite', verbose_name='utilisateurs')
+
+    class Meta:
+        verbose_name = 'Produit'
+        verbose_name_plural = 'Produits'
+
+    def __str__(self):
+        return self.product_name
+
+
+class Category(models.Model):
+    category_name = models.CharField(verbose_name='catégorie', max_length=300)
+
+    class Meta:
+        verbose_name = 'Catégorie'
+        verbose_name_plural = 'Catégories'
+    
+
+    def __str__(self):
+        return self.category_name
