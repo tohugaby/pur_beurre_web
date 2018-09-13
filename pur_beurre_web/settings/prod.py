061094efac70bb44070efa04dcf2d365f3b076ea
tom.gabriele <tom.gabriele@free.fr>
2018-09-13 10:51:21 +0200

    build: config correction

--- pur_beurre_web/settings/prod.py
+++ pur_beurre_web/settings/prod.py
@@ -27,7 +27,7 @@ MIDDLEWARE += [
 ]
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFileStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
