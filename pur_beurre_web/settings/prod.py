ad5c107858697557e1f140d39adef5be7b1a1fa7
tom.gabriele <tom.gabriele@free.fr>
2018-09-18 00:59:31 +0200

    fix: add ADMIN settings

--- pur_beurre_web/settings/prod.py
+++ pur_beurre_web/settings/prod.py
@@ -34,3 +34,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+if os.environ.get('ADMIN'):
+    ADMIN = os.environ.get('ADMIN').split(";")
