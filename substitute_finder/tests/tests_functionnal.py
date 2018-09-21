3903f26c93dfd21360755bea74d149aba71257c5
tom.gabriele <tom.gabriele@free.fr>
2018-09-22 00:19:06 +0200

    ci: add 2 more functional tests for login and logout

--- substitute_finder/tests/tests_functionnal.py
+++ substitute_finder/tests/tests_functionnal.py
@@ -1,7 +1,13 @@
+from pprint import pprint
+
+import selenium.webdriver.support.ui as ui
+from django.conf import settings
+from django.contrib.sessions.models import Session
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import selenium.webdriver.support.ui as ui
+
+from substitute_finder.models import CustomUser
 
 
 class SearchFormUserTestCase(StaticLiveServerTestCase):
@@ -51,4 +57,76 @@ class SearchFormUserTestCase(StaticLiveServerTestCase):
         self.assertTrue(self.get_element(".product-card"))
 
 
-# TODO: Missing tests: Login, Logout, clic on card, add to favorites, see favorites
+class LoginLogoutUserTestCase(StaticLiveServerTestCase):
+    fixtures = ['test_data_custom_user.json']
+
+    @classmethod
+    def setUpClass(cls):
+        super(LoginLogoutUserTestCase, cls).setUpClass()
+        cls.browser = webdriver.Firefox()
+        cls.wait = ui.WebDriverWait(cls.browser, 1000)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.browser.quit()
+        super(LoginLogoutUserTestCase, cls).tearDownClass()
+
+    @classmethod
+    def get_element(cls, css_selector: str):
+        """
+        A shortcut to get an html element by css selector.
+        :param css_selector: searched css selector
+        :type css_selector: str
+        """
+        return cls.browser.find_element_by_css_selector(css_selector)
+
+    def test_login_form_exists(self):
+        self.browser.get("%s/login" % self.live_server_url)
+        self.assertIn("Pur Beurre", self.browser.title)
+        self.wait.until(lambda driver: self.get_element("#id_email").is_displayed())
+        email_field = self.get_element("#id_email")
+        password_field = self.get_element("#id_password")
+        submit_button = self.get_element("form button")
+        email_field.send_keys("test@test.fr")
+        password_field.send_keys("test")
+        submit_button.click()
+        self.wait.until(lambda driver: self.get_element("header form #id_product").is_displayed())
+
+        session_id = self.browser.get_cookie("sessionid")['value']
+
+        session = Session.objects.get(pk=session_id)
+        user = CustomUser.objects.get(email='test@test.fr')
+
+        self.assertEqual(session.get_decoded()['_auth_user_id'], str(user.pk))
+        self.assertEqual(self.browser.current_url, "%s/" % self.live_server_url)
+        self.assertTrue(self.get_element("header form #id_product"))
+
+    def test_logout_form_exists(self):
+        self.browser.get("%s/login" % self.live_server_url)
+
+        self.wait.until(lambda driver: self.get_element("#id_email").is_displayed())
+        email_field = self.get_element("#id_email")
+        password_field = self.get_element("#id_password")
+        submit_button = self.get_element("form button")
+        email_field.send_keys("test@test.fr")
+        password_field.send_keys("test")
+        submit_button.click()
+        self.wait.until(lambda driver: self.get_element("header form #id_product").is_displayed())
+
+        session_id = self.browser.get_cookie("sessionid")['value']
+
+        session = Session.objects.get(pk=session_id)
+        user = CustomUser.objects.get(email='test@test.fr')
+
+        logout_link = self.get_element('.navbar-nav li:nth-child(3) a')
+
+        logout_link.click()
+
+        self.assertIsNone(self.browser.get_cookie("sessionid"))
+
+        self.assertEqual(self.browser.current_url, "%s/" % self.live_server_url)
+        self.assertTrue(self.get_element("header form #id_product"))
+
+
+
+# TODO: Missing tests: clic on card, add to favorites, see favorites
