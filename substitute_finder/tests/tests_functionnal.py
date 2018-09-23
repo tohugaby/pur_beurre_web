6659daeec1233864d8e53a94d97e3e7a8022d796
tom.gabriele <tom.gabriele@free.fr>
2018-09-23 23:07:11 +0200

    ci: add new functionnal tests

--- substitute_finder/tests/tests_functionnal.py
+++ substitute_finder/tests/tests_functionnal.py
@@ -10,19 +10,19 @@ from selenium.webdriver.common.keys import Keys
 from substitute_finder.models import CustomUser
 
 
-class SearchFormUserTestCase(StaticLiveServerTestCase):
-    fixtures = ['selenium.json']
+class UserInterfaceTestCase(StaticLiveServerTestCase):
+    fixtures = ['test_data_custom_user.json', 'selenium.json']
 
     @classmethod
     def setUpClass(cls):
-        super(SearchFormUserTestCase, cls).setUpClass()
+        super(UserInterfaceTestCase, cls).setUpClass()
         cls.browser = webdriver.Firefox()
         cls.wait = ui.WebDriverWait(cls.browser, 1000)
 
     @classmethod
     def tearDownClass(cls):
         cls.browser.quit()
-        super(SearchFormUserTestCase, cls).tearDownClass()
+        super(UserInterfaceTestCase, cls).tearDownClass()
 
     @classmethod
     def get_element(cls, css_selector: str):
@@ -33,7 +33,10 @@ class SearchFormUserTestCase(StaticLiveServerTestCase):
         """
         return cls.browser.find_element_by_css_selector(css_selector)
 
-    def test_header_form_exists(self):
+    def test_header_form(self):
+        """
+        Test a search using header form.
+        """
         self.browser.get(self.live_server_url)
         self.assertIn("Pur Beurre", self.browser.title)
         self.wait.until(lambda driver: self.get_element("header form").is_displayed())
@@ -45,7 +48,10 @@ class SearchFormUserTestCase(StaticLiveServerTestCase):
         self.assertEqual(self.browser.current_url, "%s/search" % self.live_server_url)
         self.assertTrue(self.get_element(".product-card"))
 
-    def test_navbar_form_exists(self):
+    def test_navbar_form(self):
+        """
+        Test a search using navbar form.
+        """
         self.browser.get(self.live_server_url)
         self.assertIn("Pur Beurre", self.browser.title)
         self.wait.until(lambda driver: self.get_element(".navbar-form").is_displayed())
@@ -56,31 +62,10 @@ class SearchFormUserTestCase(StaticLiveServerTestCase):
         self.assertEqual(self.browser.current_url, "%s/search" % self.live_server_url)
         self.assertTrue(self.get_element(".product-card"))
 
-
-class LoginLogoutUserTestCase(StaticLiveServerTestCase):
-    fixtures = ['test_data_custom_user.json']
-
-    @classmethod
-    def setUpClass(cls):
-        super(LoginLogoutUserTestCase, cls).setUpClass()
-        cls.browser = webdriver.Firefox()
-        cls.wait = ui.WebDriverWait(cls.browser, 1000)
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.browser.quit()
-        super(LoginLogoutUserTestCase, cls).tearDownClass()
-
-    @classmethod
-    def get_element(cls, css_selector: str):
+    def test_login_form(self):
         """
-        A shortcut to get an html element by css selector.
-        :param css_selector: searched css selector
-        :type css_selector: str
+        Test user login.
         """
-        return cls.browser.find_element_by_css_selector(css_selector)
-
-    def test_login_form_exists(self):
         self.browser.get("%s/login" % self.live_server_url)
         self.assertIn("Pur Beurre", self.browser.title)
         self.wait.until(lambda driver: self.get_element("#id_email").is_displayed())
@@ -101,7 +86,10 @@ class LoginLogoutUserTestCase(StaticLiveServerTestCase):
         self.assertEqual(self.browser.current_url, "%s/" % self.live_server_url)
         self.assertTrue(self.get_element("header form #id_product"))
 
-    def test_logout_form_exists(self):
+    def test_logout_form(self):
+        """
+        Test user logout.
+        """
         self.browser.get("%s/login" % self.live_server_url)
 
         self.wait.until(lambda driver: self.get_element("#id_email").is_displayed())
@@ -127,6 +115,61 @@ class LoginLogoutUserTestCase(StaticLiveServerTestCase):
         self.assertEqual(self.browser.current_url, "%s/" % self.live_server_url)
         self.assertTrue(self.get_element("header form #id_product"))
 
+    def test_visit_product_page(self):
+        """
+        Test product page.
+        """
+        self.browser.get(self.live_server_url)
+        self.assertIn("Pur Beurre", self.browser.title)
+        self.wait.until(lambda driver: self.get_element("header form").is_displayed())
+        search_field = self.get_element("header form #id_product")
+        submit_button = self.get_element("header form button")
+        search_field.send_keys("coca")
+        submit_button.click()
+        self.wait.until(lambda driver: self.get_element("#relevant_products").is_displayed())
 
+        card_link = self.get_element(".product-card a.card-body")
+        card_link.click()
 
-# TODO: Missing tests: clic on card, add to favorites, see favorites
+        product_header = self.get_element(".producthead")
+        self.assertTrue(product_header)
+
+        product_id = self.get_element(".producthead .container .row .col").get_attribute("id")
+
+        cards = self.browser.find_elements_by_css_selector(".product-card")
+        cards_id = [card.get_attribute("id") for card in cards]
+
+        self.assertNotIn(product_id, cards_id)
+
+    def test_add_to_favorites(self):
+        """
+        Test addition of product to favorites and visit favorites page.
+        """
+        self.browser.get("%s/login" % self.live_server_url)
+        self.assertIn("Pur Beurre", self.browser.title)
+        self.wait.until(lambda driver: self.get_element("#id_email").is_displayed())
+        email_field = self.get_element("#id_email")
+        password_field = self.get_element("#id_password")
+        submit_button = self.get_element("form button")
+        email_field.send_keys("test@test.fr")
+        password_field.send_keys("test")
+        submit_button.click()
+
+        self.wait.until(lambda driver: self.get_element("header form").is_displayed())
+        search_field = self.get_element("header form #id_product")
+        submit_button = self.get_element("header form button")
+        search_field.send_keys("coca")
+        submit_button.click()
+        self.wait.until(lambda driver: self.get_element("#relevant_products").is_displayed())
+
+        product_card = self.get_element(".product-card")
+        product_card_id = product_card.get_attribute("id")
+        card_add_to_favorites_link = self.get_element(".product-card .card-footer a")
+        card_add_to_favorites_link.click()
+
+        carot = self.get_element(".header-logo").find_element_by_xpath('../..')
+        carot.click()
+        self.assertIn("/favorites", self.browser.current_url)
+        cards = self.browser.find_elements_by_css_selector(".product-card")
+        cards_id = [card.get_attribute("id") for card in cards]
+        self.assertIn(product_card_id, cards_id)
