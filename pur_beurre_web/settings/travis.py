ab6197795a43c4f74133c4864ff2111f8ae5cbf9
tom.gabriele <tom.gabriele@free.fr>
2018-09-24 22:50:44 +0200

    ci: fix travis config by adding postgresql database

---
+++ pur_beurre_web/settings/travis.py
@@ -0,0 +1,13 @@
+from . import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'travis_ci_test',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '5432'
+    }
+}
