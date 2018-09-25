59d56ac6bbf71481a6ebe9241c9323e7907f1f1c
tom.gabriele <tom.gabriele@free.fr>
2018-09-25 12:37:20 +0200

    refactor: refactor api_to_db command and add unittest
    Closes https://trello.com/c/805bdAux/44-refactor-handle-method-of-django-custom-command-apitodb

--- substitute_finder/management/commands/api_to_db.py
+++ substitute_finder/management/commands/api_to_db.py
@@ -47,53 +47,31 @@ class Command(BaseCommand):
             help='enable a strict mode that delete elements with missing data'
         )
 
-    def handle(self, *args, **options):
+    def get_categories(self, from_cache: bool=False):
         """
-        Allow to get data from api or from cache and insert them into database.
+        method in charge of getting categories.
+        :param from_cache: indicates if data should be recovered from cache or not
+        :type from_cache: bool
         """
 
-        # Count initial data
-        LOGGER.info("Nb products before update: %s" % Product.objects.all().count())
-        LOGGER.info("Nb categories before update: %s" % Category.objects.all().count())
-
-        # Deal with Category
-        data = Category.get_api_data_list(from_cache=options['from_cache'])
+        data = Category.get_api_data_list(from_cache=from_cache)
         Category.insert_data(data)
 
-        # Deal with Product
-        # Define initial variables
-        data_exists = True
-        start_page = 1
-        if os.path.exists('last_page.txt'):
-            with open('last_page.txt', 'r') as lp_file:
-                start_page = int(lp_file.read())
-        if options['start_page']:
-            start_page = options['start_page']
-        actual_page = start_page
+    def get_product(self, actual_page: int=1, from_cache: bool=False, grumpy_mode: bool=False):
+        """
+        """
+        data = Product.get_api_data_list(nb_pages=1, start_page=actual_page, from_cache=from_cache)
+        Product.insert_data(data, strict_required_field_mode=grumpy_mode)
+        return bool(data)
 
-        # Loop on data recovery and integration for Product
-        while data_exists:
-            data = Product.get_api_data_list(nb_pages=1, start_page=actual_page, from_cache=options['from_cache'])
-            Product.insert_data(data, strict_required_field_mode=options['grumpy_mode'])
+    def database_cleanup(self, grumpy_mode: bool=False):
+        """
+        Clean database Product and Category tables after data recovery
+        :param grumpy_mode: indicates if database should be cleaned with a strict control on important Product fields
+        :type grumpy_mode: bool
+        """
 
-            # Check if loop should continue according to page constraints
-            data_exists = True if data else False
-            if data_exists and options['nb_pages'] and actual_page >= start_page + options['nb_pages']:
-                data_exists = False
-
-            # Prepare next page number
-            actual_page += 1
-
-            # Save last recoverd page number
-            with open('last_page.txt', 'w') as lp_file:
-                lp_file.write(str(actual_page - 1))
-
-        # Reset page counter save
-        with open('last_page.txt', 'w') as lp_file:
-            lp_file.write(str(1))
-
-        # Remove useless Category and Product instances
-        if options['grumpy_mode']:
+        if grumpy_mode:
             Product.objects.filter(Q(generic_name=None) | Q(energy_100g=None) | Q(sugars_100g=None) | Q(sodium_100g=None) |
                                    Q(carbohydrates_100g=None) | Q(salt_100g=None) | Q(proteins_100g=None) |
                                    Q(fat_100g=None) | Q(fiber_100g=None) | Q(saturated_fat_100g=None) |
@@ -106,6 +84,67 @@ class Command(BaseCommand):
         Category.objects.annotate(product_count=Count('product')).filter(product_count__lte=1).delete()
         Category.objects.filter(product__isnull=True).delete()
         Product.objects.filter(categories_tags=None).delete()
+
+    def get_recovery_state(self):
+        """
+        get recovery state from state file.
+        """
+        state = 1
+        if os.path.exists('last_page.txt'):
+            with open('last_page.txt', 'r') as lp_file:
+                state = int(lp_file.read())
+        return state
+
+    def set_recovery_state(self, page: int=1):
+        """
+        Store recovery state in file.
+        :param page: actual page
+        :type page: int
+        """
+        with open('last_page.txt', 'w') as lp_file:
+            lp_file.write(str(page))
+
+    def handle(self, *args, **options):
+        """
+        Allow to get data from api or from cache and insert them into database.
+        """
+
+        # Count initial data
+        LOGGER.info("Nb products before update: %s" % Product.objects.all().count())
+        LOGGER.info("Nb categories before update: %s" % Category.objects.all().count())
+
+        # Deal with Category
+        self.get_categories(from_cache=options['from_cache'])
+
+        # Deal with Product
+        # Define initial variables
+        data_exists = True
+        start_page = self.get_recovery_state()
+
+        if options['start_page']:
+            start_page = options['start_page']
+        actual_page = start_page
+
+        # Loop on data recovery and integration for Product
+        while data_exists:
+            data_exists = self.get_product(actual_page=actual_page,
+                                           from_cache=options['from_cache'], grumpy_mode=options['grumpy_mode'])
+
+            # Check if loop should continue according to page constraints
+            if data_exists and options['nb_pages'] and actual_page >= start_page + options['nb_pages']:
+                data_exists = False
+
+            # Prepare next page number
+            actual_page += 1
+
+            # Save last recoverd page number
+            self.set_recovery_state(page=actual_page - 1)
+
+        # Reset page counter save
+        self.set_recovery_state()
+
+        # Remove useless Category and Product instances
+        self.database_cleanup(grumpy_mode=options['grumpy_mode'])
 
         # Count final data
         LOGGER.info("Nb products after update: %s" % Product.objects.all().count())
---
+++ substitute_finder/tests/tests_api_to_db_command.py
@@ -0,0 +1,112 @@
+import os
+import json
+from io import StringIO
+from pprint import pprint
+
+from django.conf import settings
+from django.core.management import call_command
+from django.test import TestCase, override_settings
+
+from substitute_finder.models import Category, Product
+
+TEST_JSON_CACHE_DATA_PATH = os.path.join(os.path.dirname(__file__), settings.JSON_DIR_NAME)
+
+
+@override_settings(JSON_DIR_PATH=TEST_JSON_CACHE_DATA_PATH)
+class ApiToDbTestCase(TestCase):
+    MAX_NB_PAGES = 3
+
+    def setUp(self):
+        self.clean_json_files()
+
+    def get_category_json_test_file(self):
+        """
+        Recover category json test file from OpenfoodFacts
+        """
+        Category.get_api_data_list(from_cache=False)
+
+    def get_product_json_test_files(self):
+        """
+        Recover category json test file from OpenfoodFacts
+        """
+        Product.get_api_data_list(nb_pages=self.MAX_NB_PAGES, start_page=1, from_cache=False)
+
+    def clean_json_files(self):
+        """
+        Clean category file by removing useless categories. Allows faster tests.
+        """
+        try:
+            to_keep_categories = []
+
+            # Open product files to list categories to keep
+            for nb in range(1, self.MAX_NB_PAGES + 1):
+                file_path = os.path.join(TEST_JSON_CACHE_DATA_PATH, 'product_list_%s.json' % str(nb))
+                with open(file_path, 'r') as product_file:
+                    data = json.loads(product_file.read())
+                    for product in data:
+                        if 'categories_tags' in product.keys():
+                            for category in product['categories_tags']:
+                                to_keep_categories.append(category)
+            to_keep_categories = set(to_keep_categories)
+
+            # Read actual category list and clean it.
+            categories = []
+            with open(os.path.join(TEST_JSON_CACHE_DATA_PATH, 'category_list.json'), 'r') as category_file:
+                categories = json.loads(category_file.read())
+            new_categories = list(filter(lambda cat: cat['id'] in to_keep_categories, categories))
+            with open(os.path.join(TEST_JSON_CACHE_DATA_PATH, 'category_list.json'), 'w') as new_category_file:
+                new_category_file.write(json.dumps(new_categories))
+
+        # recover test file if missing or if empty
+        except (FileNotFoundError, json.decoder.JSONDecodeError) as file_or_json_exc:
+
+            self.get_category_json_test_file()
+            self.get_product_json_test_files()
+            self.clean_json_files()
+
+        except (KeyError, Exception) as other_exc:
+            raise other_exc
+
+    def test_api_to_db_with_nb_pages(self):
+        self.assertEqual(Category.objects.count(), 0)
+        self.assertEqual(Product.objects.count(), 0)
+        out = StringIO()
+        options = {
+            'start_page': 1,
+            'nb_pages': 2,
+            'from_cache': True,
+            'grumpy_mode': False
+        }
+        call_command('api_to_db', stdout=out, **options)
+        self.assertNotEqual(Category.objects.count(), 0)
+        self.assertNotEqual(Product.objects.count(), 0)
+
+    def test_api_to_db_with_grumpy_mode(self):
+        before_nb_cat = Category.objects.count()
+        before_nb_prod = Product.objects.count()
+        self.assertEqual(before_nb_cat, 0)
+        self.assertEqual(before_nb_prod, 0)
+
+        out = StringIO()
+        options = {
+            'start_page': 1,
+            'nb_pages': 2,
+            'from_cache': True,
+            'grumpy_mode': False
+        }
+        call_command('api_to_db', stdout=out, **options)
+        no_grumpy_mode_nb_cat = Category.objects.count()
+        no_grumpy_mode_nb_prod = Product.objects.count()
+        self.assertNotEqual(no_grumpy_mode_nb_cat, 0)
+        self.assertNotEqual(no_grumpy_mode_nb_prod, 0)
+
+        options['grumpy_mode'] = True
+
+        call_command('api_to_db', stdout=out, **options)
+        grumpy_mode_nb_cat = Category.objects.count()
+        grumpy_mode_nb_prod = Product.objects.count()
+        self.assertNotEqual(grumpy_mode_nb_cat, 0)
+        self.assertNotEqual(grumpy_mode_nb_prod, 0)
+
+        self.assertTrue(no_grumpy_mode_nb_cat >= grumpy_mode_nb_cat >= before_nb_cat)
+        self.assertTrue(no_grumpy_mode_nb_prod >= grumpy_mode_nb_prod >= before_nb_prod)
