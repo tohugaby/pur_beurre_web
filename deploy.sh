cb792f22014599e405efad2b0835b816f06fa124
tom.gabriele <tom.gabriele@free.fr>
2018-11-15 22:40:29 +0100

    feat: update deployment script
    5bd4691cced2c01d7166a0ee set travis config

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+cd ~/pur_beurre_web
 # Change permissions to something that SSH accepts
 chmod 600 deploy/deploy_key;
 # Send the commit hash env variable over ssh to know which commit to checkout to
 export LC_COMMIT_HASH="$TRAVIS_COMMIT"
 # Pipe the update script over SSH to the production server
-cat make docker-deploy | ssh -o StrictHostKeyChecking=no -o BatchMode=yes -o SendEnv=LC_COMMIT_HASH -i
+make docker-deploy | ssh -o StrictHostKeyChecking=no -o BatchMode=yes -o SendEnv=LC_COMMIT_HASH -i
 deploy/deploy_key "$DEPLOY_USER@$DEPLOY_SERVER"
