eee1c6068f3b21bbf95bda3f74c82a86e63c249f
tom.gabriele <tom.gabriele@free.fr>
2018-11-15 22:33:49 +0100

    feat: update travis config and deployment script
    5bd4691cced2c01d7166a0ee set travis config

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
-all: test deploy
+all: test docker-deploy
 
 test:
 	@echo "Start tests"
 	@python manage.py test --settings pur_beurre_web.settings.test
 	@echo "Tests finished"
 
-deploy:
+docker-deploy:
 	@echo "Start docker deployment"
 	@chmod +x docker_deploy.sh
 	@./docker_deploy.sh
--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,9 @@
-#!/usr/bin/env bash
-echo COUCOUCOUCOUCOU
+#!/bin/bash
+
+# Change permissions to something that SSH accepts
+chmod 600 deploy/deploy_key;
+# Send the commit hash env variable over ssh to know which commit to checkout to
+export LC_COMMIT_HASH="$TRAVIS_COMMIT"
+# Pipe the update script over SSH to the production server
+cat make docker-deploy | ssh -o StrictHostKeyChecking=no -o BatchMode=yes -o SendEnv=LC_COMMIT_HASH -i
+deploy/deploy_key "$DEPLOY_USER@$DEPLOY_SERVER"
