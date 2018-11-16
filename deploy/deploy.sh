c759128ee2f28b23d750cec53c0b551b0092a58c
tom.gabriele <tom.gabriele@free.fr>
2018-11-17 00:36:28 +0100

    fix: reorganize travis deployment scripts
    5bd4691cced2c01d7166a0ee set travis config

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,4 +6,5 @@ chmod 600 deploy/deploy_key;
 # Send the commit hash env variable over ssh to know which commit to checkout to
 export LC_COMMIT_HASH="$TRAVIS_COMMIT"
 # Pipe the update script over SSH to the production server
-chmod +x deploy/update_app.sh && cat deploy/update_app.sh | ssh -o StrictHostKeyChecking=no -o BatchMode=yes -o SendEnv=LC_COMMIT_HASH -i deploy_key "$DEPLOY_USER@$DEPLOY_SERVER"
+chmod +x deploy/update_app.sh && cat deploy/update_app.sh | ssh -o StrictHostKeyChecking=no -o BatchMode=yes \
+-o SendEnv=LC_COMMIT_HASH -i deploy/deploy_key "$DEPLOY_USER@$DEPLOY_SERVER"
