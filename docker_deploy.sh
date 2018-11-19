9a7b45c1b11f2c6fcbedf884777f1c931f1725f4
tom.gabriele <tom.gabriele@free.fr>
2018-11-19 23:15:15 +0100

    feat: add cron task setter in deploy script
    5bd46957f3aa0a2c5a5b52f8 Set cron task
    Closes https://trello.com/c/FGJmziZm/6-set-cron-task

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -3,7 +3,10 @@
 echo "Remove previous containers"
 docker-compose down
 echo "Remove purbeurre image"
-docker rmi purbeurreweb_purbeurre:latest
+docker rmi purbeurre:latest
 echo "Build and launch containers"
-pwd
-docker-compose up -d
+docker-compose up --build -d purbeurre
+echo "Set update db cron task"
+ACTUAL_DIR="$(pwd)"
+echo "55 23 * * 0 cd ${ACTUAL_DIR} && docker-compose up --build purbeurre_update" > purbeurre_cron
+crontab purbeurre_cron
